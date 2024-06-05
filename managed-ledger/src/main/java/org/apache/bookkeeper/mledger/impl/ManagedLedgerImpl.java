5c9cab0b135cd105c6948919830908fe98b587b9
Chayan Pathak <chayanpathak2013@gmail.com>
2024-06-05 09:31:07 +0530

    retrying from initial commit

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
@@ -1,20 +1,16 @@
 package org.apache.bookkeeper.mledger.impl;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.NavigableMap;
-import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.logging.Logger; 
 import org.apache.bookkeeper.mledger.ManagerLedger;
 
 public class ManagedLedgerImpl implements ManagedLedger {
 
+    enum State {
+        Open,
+        Closed,
+        Fenced
+    }
 
-    // this is the central repository of ledgers being managed
-    protected MetaStore centralRecordStore;
-    protected final NavigableMap<Long, LedgerInfo> ledgers = ConcurrentSkipListMap<>();
-    protected List<ManagedCursor> waitingManagedCursors;
-    protected List<ManagedCursor> 
+    protected final BookKeeper bookKeeper;
     public ManagedLedgerImpl() {
     }
 
@@ -22,21 +18,6 @@ public class ManagedLedgerImpl implements ManagedLedger {
     public void initialize() {
     }
 
-    // Start the Bookkeeper store unless it exists
     protected void initializeBookKeeper() {
-        // Calculate the total size of the ledgers
-        List<Long> emptyLedgersToBeDeleted = Collections.synchronizedList(new ArrayList<>());
-
-        Iterator<LedgerInfo> iterator = ledgers.values().iterator();
-        int TotalLedgerSize = 0;
-        while(iterator.hasNext()) {
-            LedgerInfo li = iterator.next();
-            TotalLedgerSize += li.getSize();
-            if (li.getNumberOfEntries() == 0) {
-                emptyLedgersToBeDeleted.add(li.getLedgerId());
-                iterator.remove();
-            }
-
-        }
     }
 }
