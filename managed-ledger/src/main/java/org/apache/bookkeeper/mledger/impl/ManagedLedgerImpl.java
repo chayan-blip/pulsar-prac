b54c00d6f1781e3305acf1b425273dbb448a6872
Chayan Pathak <chayanpathak2013@gmail.com>
2024-07-18 22:25:09 +0530

    Added Mledger enum states

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
@@ -5,9 +5,17 @@ import org.apache.bookkeeper.mledger.ManagerLedger;
 public class ManagedLedgerImpl implements ManagedLedger {
 
     enum State {
-        Open,
-        Closed,
-        Fenced
+        None,
+        LedgerOpened,
+        ClosingLedger, // Closing Current Ledger
+        ClosedLedger,  // Current ledger has been closed and there's no pending 
+                       // operation
+        CreatingLedger, // Creating a new Ledger
+        Closed, // ManagedLedger has been closed
+        Fenced, // A managed ledger is fenced when there is some concurrent
+                // access from a different session/machine. In this state the 
+                // managed ledger will throw an exception for all operations, since
+                // the new instance will take over
     }
 
     protected final BookKeeper bookKeeper;
